ee0dfba5bb1c47f0ba4f139eadcb5609c783eaec
jcwherington <jakecwherington@gmail.com>
2025-02-20 23:08:37 +0000

    update local env config and dependency installs

--- services/notification/scripts/local.sh
+++ services/notification/scripts/local.sh
@@ -10,5 +10,8 @@ function on_error
 
 trap on_error ERR
 
+STAGE=local
+SENDER=$(aws ssm get-parameter --name mpv2-sender --query "Parameter.Value" --output text)
+
 docker build -t notification-lambda:local .
-docker run -v ~/.aws:/root/.aws -p 9000:8080 notification-lambda:local
+docker run -v ~/.aws:/root/.aws -e STAGE=$STAGE -e SENDER=$SENDER -p 9000:8080 notification-lambda:local

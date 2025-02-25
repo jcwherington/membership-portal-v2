65d73a25ccb1d3fbfcd5c26c6ae4556cd6cd1d49
jcwherington <jakecwherington@gmail.com>
2025-02-25 19:00:01 +0000

    switch stage env var to branch

--- services/notification/scripts/local.sh
+++ services/notification/scripts/local.sh
@@ -10,8 +10,8 @@ function on_error
 
 trap on_error ERR
 
-STAGE=local
+BRANCH=local
 SENDER=$(aws ssm get-parameter --name mpv2-sender --query "Parameter.Value" --output text)
 
 docker build --target local -t notification:local .
-docker run -v ~/.aws:/root/.aws -e STAGE=$STAGE -e SENDER=$SENDER -p 9000:8080 notification:local
+docker run -v ~/.aws:/root/.aws -e BRANCH=$BRANCH -e SENDER=$SENDER -p 9000:8080 notification:local
--- services/notification/src/config.rb
+++ services/notification/src/config.rb
@@ -6,8 +6,8 @@ def region
     'us-west-2'
 end
 
-def stage
-    ENV['STAGE']
+def branch
+    ENV['BRANCH']
 end
 
 def sender
@@ -19,11 +19,11 @@ def template_bucket
 end
 
 def local
-    ENV['STAGE'] == 'local'
+    ENV['BRANCH'] == 'local'
 end
 
 def template_path
-    local() ? './outcome_email.html' : "outcome_email_#{stage()}"
+    local() ? './outcome_email.html' : "outcome_email_#{branch()}"
 end
 
 def logger
